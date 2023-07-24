a18a193262160fd15ae769b58489b322aa13846e
Kyrylo Lysenko <lysenko.ko@gmail.com>
2023-07-24 22:51:34 +0300

    Update by using React Hooks

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,43 +1,40 @@
 import PropTypes from 'prop-types';
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import css from './Modal.module.css';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export class Modal extends Component {
-  //Вішаемо слухача на подію 'keydown' при монтуванні компонента модального вікна
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
-  //Видаляємо слухач на подію 'keydown' при розмонтуванні компонента модального вікна
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
-
-  //Метод закриття модального вікна по Escape
-  handleKeyDown = e => {
+export const Modal = ({ children, onClose }) => {
+  //Функція закриття модального вікна по Escape
+  const handleKeyDown = e => {
     if (e.code === 'Escape') {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  //Метод закриття модального по кліку на backdrop
-  handleBackdropClick = e => {
+  //Функція закриття модального по кліку на backdrop
+  const handleBackdropClick = e => {
     if (e.target === e.currentTarget) {
-      this.props.onClose();
+      onClose();
     }
   };
+  //Після монтування компонента - додаємо слухача події keydown. При розмонтуванні - видаляємо цей слухач.
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
   //Компонент модального вікна буде рендеритись в окремий портал
-  render() {
-    return createPortal(
-      <div className={css.overlay} onClick={this.handleBackdropClick}>
-        <div className={css.modal}>{this.props.children}</div>
-      </div>,
-      modalRoot
-    );
-  }
-}
+  return createPortal(
+    <div className={css.overlay} onClick={handleBackdropClick}>
+      <div className={css.modal}>{children}</div>
+    </div>,
+    modalRoot
+  );
+};
 
 Modal.propTypes = {
   onCLose: PropTypes.func,
