af9f9f6f783edfd08e900fb9772586820b691ef0
Kyrylo Lysenko <lysenko.ko@gmail.com>
2023-07-25 00:54:20 +0300

    Fix useEffect error

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -6,26 +6,26 @@ import css from './Modal.module.css';
 const modalRoot = document.querySelector('#modal-root');
 
 export const Modal = ({ children, onClose }) => {
-  //Функція закриття модального вікна по Escape
-  const handleKeyDown = e => {
-    if (e.code === 'Escape') {
-      onClose();
-    }
-  };
-
   //Функція закриття модального по кліку на backdrop
   const handleBackdropClick = e => {
     if (e.target === e.currentTarget) {
       onClose();
     }
   };
+
   //Після монтування компонента - додаємо слухача події keydown. При розмонтуванні - видаляємо цей слухач.
   useEffect(() => {
+    //Функція закриття модального вікна по Escape
+    const handleKeyDown = e => {
+      if (e.code === 'Escape') {
+        onClose();
+      }
+    };
     window.addEventListener('keydown', handleKeyDown);
     return () => {
       window.removeEventListener('keydown', handleKeyDown);
     };
-  }, []);
+  }, [onClose]);
 
   //Компонент модального вікна буде рендеритись в окремий портал
   return createPortal(
