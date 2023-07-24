332180220f4da12013efceb8681f5a746a2ffa15
Kyrylo Lysenko <lysenko.ko@gmail.com>
2023-07-24 23:07:36 +0300

    Update by using React Hooks

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,50 +1,47 @@
-import { Component } from 'react';
-
+import { useState } from 'react';
 import toast from 'react-hot-toast';
 import { FcSearch } from 'react-icons/fc';
 import PropTypes from 'prop-types';
 import css from './Searchbar.module.css';
 
-export class Searchbar extends Component {
-  state = {
-    query: '',
+export const Searchbar = ({ onSubmit }) => {
+  const [query, setQuery] = useState('');
+
+  //Функція, що зберігає поточне значення імпута у змінній query
+  const handleImputChange = event => {
+    setQuery(event.currentTarget.value.toLowerCase());
   };
 
-  handleImputChange = event => {
-    this.setState({ query: event.currentTarget.value.toLowerCase() });
-  };
-
-  handleSubmit = event => {
+  //Функція, що виконує сабміт форми. Значення query передається вгору в бітьківський компонент App. Якщо імпут порожній - з'являється повідомлення.
+  const handleSubmit = event => {
     event.preventDefault();
-    if (this.state.query.trim() === '') {
+    if (query.trim() === '') {
       toast.error('You didnt enter anything');
       return;
     }
-    this.props.onSubmit(this.state.query);
-    this.setState({ query: '' });
+    onSubmit(query);
+    setQuery('');
   };
 
-  render() {
-    return (
-      <header className={css.searchBar}>
-        <form onSubmit={this.handleSubmit} className={css.searchForm}>
-          <button type="submit" className={css.searchFormButton}>
-            <FcSearch size={'2em'} />
-          </button>
-          <input
-            className={css.searchFormInput}
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            value={this.state.query}
-            onChange={this.handleImputChange}
-          />
-        </form>
-      </header>
-    );
-  }
-}
+  return (
+    <header className={css.searchBar}>
+      <form onSubmit={handleSubmit} className={css.searchForm}>
+        <button type="submit" className={css.searchFormButton}>
+          <FcSearch size={'2em'} />
+        </button>
+        <input
+          className={css.searchFormInput}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          value={query}
+          onChange={handleImputChange}
+        />
+      </form>
+    </header>
+  );
+};
 
 Searchbar.propTypes = {
   onSubmit: PropTypes.func.isRequired,
