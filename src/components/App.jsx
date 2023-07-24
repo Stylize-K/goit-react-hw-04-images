746356db866f080aec62e83716c9b5df227364e4
Kyrylo Lysenko <lysenko.ko@gmail.com>
2023-07-25 00:35:48 +0300

    Update by using React Hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { ThreeCircles } from 'react-loader-spinner';
 import toast, { Toaster } from 'react-hot-toast';
 import { ImageGallery } from './ImageGallery/ImageGallery';
@@ -9,112 +9,104 @@ import { Button } from './Button/Button';
 import { Modal } from './Modal/Modal';
 import css from './App.module.css';
 
-export class App extends Component {
-  state = {
-    query: '',
-    images: [],
-    page: 1,
-    modalImageURL: '',
-    isLoading: false,
-    showModal: false,
-    endCollection: false,
-    tags: '',
+export const App = () => {
+  const [query, setQuery] = useState('');
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [modalImageURL, setModalImageURL] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [endCollection, setEndCollection] = useState(false);
+  const [tags, setTags] = useState('');
+
+  //Функуція відкриття модального вікна
+  const openModal = (url, tags) => {
+    setShowModal(true);
+    setModalImageURL(url);
+    setTags(tags);
   };
 
-  //Метод відкриття модального вікна
-  openModal = (url, tags) => {
-    this.setState({
-      showModal: true,
-      modalImageURL: url,
-      tags,
-    });
+  //Функція закриття модального вікна
+  const closeModal = () => {
+    setShowModal(false);
+    setModalImageURL('');
+    setTags('');
   };
 
-  //Метод закриття модального вікна
-  closeModal = () => {
-    this.setState({
-      showModal: false,
-      modalImageURL: '',
-      tags: '',
-    });
+  //Функція обробки сабміту форми - додаємо дані в state (дані отримуємо з компонента serchBar)
+  const handleFormSubmit = query => {
+    setQuery(query);
+    setPage(1);
+    setImages([]);
+    setEndCollection(false);
   };
 
-  //Метод обробки сабміту форми - додаємо дані в state (дані отримуємо з компонента serchBar)
-  handleFormSubmit = query => {
-    this.setState({ query, page: 1, images: [], endCollection: false });
+  //Функція обробки кліка по кнопці "Load more"
+  const handleLoadMore = () => {
+    setPage(prev => prev + 1);
   };
 
-  //Метод обробки кліку на кнопку "Load more"
-  handleLoadMore = () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }));
-  };
-
-  async componentDidUpdate(prevProps, prevState) {
-    const { query, page } = this.state;
-    if (prevState.query !== query || prevState.page !== page) {
+  //При кожній зміні значень query та page - виконуємо запит на backend.
+  useEffect(() => {
+    //Уникаємо роботи useEffeсt при першому рендері
+    if (query === '') {
+      return;
+    }
+    const fetchData = async () => {
       try {
-        this.setState({ isLoading: true });
-
+        setIsLoading(true);
         const data = await fetchImages(query, page);
-
-        this.setState(prevState => ({
-          images: [...prevState.images, ...data.hits],
-        }));
-
+        setImages(prev => [...prev, ...data.hits]);
         console.log(data);
-
         if (!data.totalHits) {
           return toast.success(
             'Sorry, there are no images matching your search query. Please try again'
           );
         }
-
         const totalPages = Math.ceil(data.totalHits / 12);
-
         if (page === totalPages) {
-          this.setState({ endCollection: true });
+          setEndCollection(true);
           toast.success('No more pictures');
         }
       } catch (error) {
         console.log('Error', error.message);
       } finally {
-        this.setState({ isLoading: false });
+        setIsLoading(false);
       }
-    }
-  }
+    };
+    fetchData();
+  }, [query, page]);
 
-  render() {
-    const { images, isLoading, endCollection, showModal, modalImageURL } =
-      this.state;
-    const showLoadMoreBtn = images.length > 0 && !endCollection;
-    return (
-      <div className={css.app}>
-        <Toaster position="top-right" reverseOrder={false} />
-        {showModal && (
-          <Modal onClose={this.closeModal}>
-            <img src={modalImageURL} alt={this.state.tags} />
-          </Modal>
-        )}
-        <Searchbar onSubmit={this.handleFormSubmit} />
-        <ImageGallery images={this.state.images} onClick={this.openModal} />
-        {showLoadMoreBtn && <Button onClick={() => this.handleLoadMore()} />}
-        {isLoading && (
-          <Loader>
-            <ThreeCircles
-              height="100"
-              width="100"
-              color="#063970"
-              wrapperStyle={{}}
-              wrapperClass=""
-              visible={true}
-              ariaLabel="three-circles-rotating"
-              outerCircleColor=""
-              innerCircleColor=""
-              middleCircleColor=""
-            />
-          </Loader>
-        )}
-      </div>
-    );
-  }
-}
+  //Умова для рендеру кнопку "Load more" => коли масив отриманих картинок не пустий и це не кінець колекції.
+  const showLoadMoreBtn = images.length > 0 && !endCollection;
+
+  return (
+    <div className={css.app}>
+      <Toaster position="top-right" reverseOrder={false} />
+      {showModal && (
+        <Modal onClose={closeModal}>
+          <img src={modalImageURL} alt={tags} />
+        </Modal>
+      )}
+      <Searchbar onSubmit={handleFormSubmit} />
+      <ImageGallery images={images} onClick={openModal} />
+      {showLoadMoreBtn && <Button onClick={() => handleLoadMore()} />}
+      {isLoading && (
+        <Loader>
+          <ThreeCircles
+            height="100"
+            width="100"
+            color="#063970"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+            ariaLabel="three-circles-rotating"
+            outerCircleColor=""
+            innerCircleColor=""
+            middleCircleColor=""
+          />
+        </Loader>
+      )}
+    </div>
+  );
+};
