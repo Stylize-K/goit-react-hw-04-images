bbd1d9887ffe49744dda84c31e8e093484e3a556
Kyrylo Lysenko <lysenko.ko@gmail.com>
2023-07-26 14:56:29 +0300

    Refactor GalleryItem

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -5,33 +5,23 @@ import css from './ImageGalleryItem.module.css';
 
 export const ImageGalleryItem = ({ image }) => {
   const [showModal, setShowModal] = useState(false);
-  const [tags, setTags] = useState('');
-  const [modalImageURL, setModalImageURL] = useState('');
 
-  //Функуція відкриття модального вікна
-  const openModal = (url, tags) => {
-    setShowModal(true);
-    setModalImageURL(url);
-    setTags(tags);
+  //Функція-toggle модального вікна
+  const toggleModal = () => {
+    setShowModal(prev => !prev);
   };
 
-  //Функція закриття модального вікна
-  const closeModal = () => {
-    setShowModal(false);
-    setModalImageURL('');
-    setTags('');
-  };
   return (
     <li className={css.imageGalleryItem}>
       <img
         className={css.imageGalleryItemImage}
         src={image.webformatURL}
         alt={image.tags}
-        onClick={() => openModal(image.largeImageURL, image.tags)}
+        onClick={() => toggleModal()}
       />
       {showModal && (
-        <Modal onClose={closeModal}>
-          <img src={modalImageURL} alt={tags} />
+        <Modal onClose={toggleModal}>
+          <img src={image.largeImageURL} alt={image.tags} />
         </Modal>
       )}
     </li>
